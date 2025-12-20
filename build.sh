04d4edf4d5fa26c2e0554f93649b36ab3e8cec55
Chris Robertson <electronicsleep@gmail.com>
2025-12-20 10:33:29 -0800

    multi-build-arch

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 gofmt -s -w .
 mkdir -p bin
-GOOS=linux GOARCH=amd64 go build -o bin/gomon gomon.go
+GOOS=linux GOARCH=amd64 go build -o bin/gomon_amd64 gomon.go
+GOOS=linux GOARCH=arm go build -o bin/gomon_arm gomon.go
 go build gomon.go
